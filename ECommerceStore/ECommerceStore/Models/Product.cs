55d7b9d56d10600546133001b16977ce49d62afc
AndrewBaik <andrewbaik@outlook.com>
2018-07-10 14:47:12 -0700

    Seeded initial 10 products into database

--- ECommerceStore/ECommerceStore/Models/Product.cs
+++ ECommerceStore/ECommerceStore/Models/Product.cs
@@ -11,7 +11,7 @@ namespace ECommerceStore.Models
         public string SKU { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Descrition { get; set; }
+        public string Description { get; set; }
         public string Image { get; set; }
     }
 }
