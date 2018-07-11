5d1d438296a2f1a137187f36fbeb14e2324f7126
AndrewBaik <andrewbaik@outlook.com>
2018-07-11 10:50:17 -0700

    add new tst class

---
+++ ECommerceStore/ECommerceStore/Models/TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceStore.Models
+{
+    public class TestClass
+    {
+        public int Name { get; set; }
+    }
+}
