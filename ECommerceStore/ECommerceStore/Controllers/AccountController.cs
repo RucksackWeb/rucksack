72747b8843c7f443aa4212e5821a8f42f4c0f6a3
AndrewBaik <andrewbaik@outlook.com>
2018-07-11 11:51:29 -0700

    User registration functionality in place

---
+++ ECommerceStore/ECommerceStore/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerceStore.Models;
+using ECommerceStore.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel rvm)
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Email = rvm.Email,
+                FirstName = rvm.FirstName,
+                LastName = rvm.LastName
+            };
+
+            var result = await _userManager.CreateAsync(user, rvm.Password);
+
+            if (result.Succeeded)
+            {
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
